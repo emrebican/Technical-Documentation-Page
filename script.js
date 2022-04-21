3c677ddf7e695942f6bb1510427da5ba2696409d
Emre Bican <emrebican11@hotmail.com>
2022-04-21 20:58:24 +0300

    changed

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let element = document.getElementById('buton');
+element.addEventListener('click', funcOne);
+
+let ground = document.getElementById('main');
+
+function funcOne(e) {
+    e.preventDefault();
+    if (ground.style.backgroundColor == "lightgrey") {
+    document.body.style.backgroundColor = "gray";
+    document.body.style.color = "white";
+    ground.style.backgroundColor = "#0b0a14";
+    ground.style.color = "white";
+    } else {
+    ground.style.backgroundColor = "lightgrey";
+    ground.style.color = "black";
+    }
+};
